f5bf7496f4689be0cbdc4c5af23fe62af991d160
Vishesh Kumar <vishesh@visheshk.net>
2016-06-14 00:46:55 -0500

    Cleaning up sensitive data

--- dash/lib/config.js
+++ dash/lib/config.js
@@ -1,11 +1,8 @@
 Meteor.startup(() => {
 	// console.log(process.env.MONGO_URL);
-	// console.log("cof");
 	// export MONGO_URL = "mongodb://127.0.0.1:27017/local";
 	// process.env.MONGO_URL = "mongodb://" + mongo_user + ":" + mongo_pass + "@" +  mongo_ip + ":27017/cspace";
 });
 
 // rests = new Mongo.Collection("restaurants");
 // console.log("mongodb://" + mongo_user + ":" + mongo_pass + "@" +  mongo_ip + ":27017/cspace");
-// var db = new MongoInternals.RemoteCollectionDriver("mongodb://" + mongo_user + ":" + mongo_pass + "@" +  mongo_ip + ":27017/cspace");
-// Members = new Mongo.Collection("Members", {_driver: db});
